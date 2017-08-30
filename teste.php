91ae45cd85b76c6353041dec5ab0066b135abcb7
jardielPC <jfcjardiel@gmail.com>
2017-08-30 19:53:38 -0300

    apenas referenciando o novo.js

--- teste.php
+++ teste.php
@@ -44,13 +44,6 @@
     <!-- somente para essa pagina -->
     <link rel="stylesheet" type="text/css" href="style_dispositivo.css">
 
-    <script>
-    function form_maker(str){
-    alert(str);
-    }
-
-    </script>
-
   </head>
   <body>
     <!-- SCROLL TOP BUTTON -->
@@ -252,31 +245,6 @@
       <!-- End footer bottom area -->
 
     </footer>
-
-    <!-- Javascript Files
-    ================================================== -->
-
-    <!-- initialize jQuery Library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <!-- Preloader js file -->
-    <script src="js/queryloader2.min.js" type="text/javascript"></script>
-    <!-- For smooth animatin  -->
-    <script src="js/wow.min.js"></script>  
-    <!-- Bootstrap js -->
-    <script src="js/bootstrap.min.js"></script>
-    <!-- slick slider -->
-    <script src="js/slick.min.js"></script>
-    <!-- superslides slider -->
-    <script src="js/jquery.easing.1.3.js"></script>
-    <script src="js/jquery.animate-enhanced.min.js"></script>
-    <script src="js/jquery.superslides.min.js" type="text/javascript" charset="utf-8"></script>   
-    <!-- for circle counter -->
-    <script src='https://cdn.rawgit.com/pguso/jquery-plugin-circliful/master/js/jquery.circliful.min.js'></script>
-    <!-- Gallery slider -->
-    <script type="text/javascript" language="javascript" src="js/jquery.tosrus.min.all.js"></script>   
-   
-    <!-- Custom js-->
-    <script src="js/custom.js"></script>
     
     <!-- Javascript usado para embarcar php  -->
     <script type="text/javascript" language="javascript" src="js/novo.js"></script>
