61372282b17bc600efe8ecffd09c22bb1e9941f2
jardielPC <jfcjardiel@gmail.com>
2017-08-30 15:31:30 -0300

    adicionando o jquery2 no teste.php

--- teste.php
+++ teste.php
@@ -50,7 +50,7 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <script src="js/jquery.min.js"></script>
+    <script src="js/jquery2.min.js"></script>
     <script>
       $(document).ready(function() {
           $('.dispositivo_select').change(function () {
