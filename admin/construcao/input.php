e496eb7cd29ece28b155f0717a0c124985a67bf6
jardielPC <jfcjardiel@gmail.com>
2017-08-31 02:32:41 -0300

    criando teste para ajax

---
+++ admin/construcao/input.php
@@ -0,0 +1,57 @@
+<?php
+// get the q parameter from URL
+$id = $_REQUEST["id"];
+$mysqli = new mysqli('localhost', 'root', 'input212', 'input');
+
+// Oh no! A connect_errno exists so the connection attempt failed!
+if ($mysqli->connect_errno) {
+    // The connection failed. What do you want to do? 
+    // You could contact yourself (email?), log the error, show a nice page, etc.
+    // You do not want to reveal sensitive information
+
+    // Let's try this:
+    echo "Sorry, this website is experiencing problems.";
+
+    // Something you should not do on a public site, but this example will show you
+    // anyways, is print out MySQL error related information -- you might log this
+    echo "Error: Failed to make a MySQL connection, here is why: \n";
+    echo "Errno: " . $mysqli->connect_errno . "\n";
+    echo "Error: " . $mysqli->connect_error . "\n";
+    
+    // You might want to show them something nice, but we will simply exit
+    exit;
+}
+
+// Perform an SQL query
+$sql = "SELECT * FROM config_dispositivo_atributos WHERE id_dispositivo=" . $id . "ORDER BY id_config";
+if (!$result = $mysqli->query($sql)) {
+    // Oh no! The query failed. 
+    echo "Sorry, the website is experiencing problems.";
+
+    // Again, do not do this on a public site, but we'll show you how
+    // to get the error information
+    echo "Error: Our query failed to execute and here is why: \n";
+    echo "Query: " . $sql . "\n";
+    echo "Errno: " . $mysqli->errno . "\n";
+    echo "Error: " . $mysqli->error . "\n";
+    exit;
+}
+
+// Phew, we made it. We know our MySQL connection and query 
+// succeeded, but do we have a result?
+if ($result->num_rows === 0) {
+    // Oh, no rows! Sometimes that's expected and okay, sometimes
+    // it is not. You decide. In this case, maybe actor_id was too
+    // large? 
+    echo "We could not find a match for ID $aid, sorry about that. Please try again.";
+    exit;
+}
+
+while ($dispositivo = $result->fetch_assoc()) {
+    echo dispositivo['nome_atributo'];
+}
+
+// The script will automatically free the result and close the MySQL
+$result->free();
+$mysqli->close();
+?>
