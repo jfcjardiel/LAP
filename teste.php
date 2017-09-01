445432771f337c5eba2097f246fe8569f5aca5d4
jardielPC <jfcjardiel@gmail.com>
2017-09-01 11:30:01 -0300

    tentando answer responder em teste.php

--- teste.php
+++ teste.php
@@ -113,7 +113,27 @@
         }
         //Caso nao haja problemas de preenchimento
         if(is_form_ok){
-          sendForm(id_dispositivo_select);
+          //stablihing new XMLHttpRequest
+          var xhttp;
+          xhttp = new XMLHttpRequest();
+          //vendo o tamanho da form
+          var form_length = document.forms["form_dispositivo"].length;
+          var aux_send = 0;
+          //building the URL that will be send
+          $url_send_form = "disp_form/answer.php?id_dispositivo="+id_dispositivo_select;
+          while(aux_send < form_length){
+            $url_send_form = $url_send_form + "&valor" + aux_send + "=" + document.getElementById(aux_send).value;
+            aux_send = aux_send + 1;
+          }
+          //inicializing the request
+          xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+              document.getElementById("answer_form").innerHTML = this.responseText;
+            }
+          };
+          //sending the data to answer.
+          xhttp.open("GET", $url_send_form, true);
+          xhttp.send();   
         }
       }
 
