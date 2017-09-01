d5ecb1ec377f3952ff1c4a677f9034406d1ce4f7
jardielPC <jfcjardiel@gmail.com>
2017-09-01 11:36:14 -0300

    tentando answer responder em teste.php

--- teste.php
+++ teste.php
@@ -122,7 +122,7 @@
           //building the URL that will be send
           url_send_form = "disp_form/answer.php?id_dispositivo="+id_dispositivo_select;
           while(aux_send < form_length){
-            $url_send_form = $url_send_form + "&valor" + aux_send + "=" + document.getElementById(aux_send).value;
+            url_send_form = url_send_form + "&valor" + aux_send + "=" + document.getElementById(aux_send).value;
             aux_send = aux_send + 1;
           }
           //inicializing the request
