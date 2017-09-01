696c30f43e50a11e84061b71cf21c1f3154d85e2
jardielPC <jfcjardiel@gmail.com>
2017-09-01 11:16:53 -0300

    tentando answer responder em teste.php

--- disp_form/input.php
+++ disp_form/input.php
@@ -79,7 +79,7 @@ while ($config_dispositivo = $result->fetch_assoc()) {
 
 //writting submit button
 //we are going to send the id of the form via the function validateFom.
-echo "<button type='submit' name='button_submit' id='button_submit' value='button_submit' onclick='validateForm(" . $id .")'>Calculate</button>";
+echo "<button type='button' name='button_submit' id='button_submit' value='button_submit' onclick='validateForm(" . $id .")'>Calculate</button>";
 
 //we should close the connection
 $mysqli->close();
