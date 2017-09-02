4dcbf4db0b1e1090bd0366a8dd5b3855a5c77a48
jardielPC <jfcjardiel@gmail.com>
2017-09-02 03:43:05 -0300

    tentando answer responder em teste.php

--- teste.php
+++ teste.php
@@ -71,11 +71,10 @@
         var xhttp;
         xhttp = new XMLHttpRequest();
         //vendo o tamanho da form
-        var form_length = document.forms["form_dispositivo"].length;
-        var aux_send = 0;
+        var form_length = document.forms["form_dispositivo"].length-1;
         //building the URL that will be send
         $url_send_form = "disp_form/answer.php?id_dispositivo="+id_dispositivo_select;
-        while(aux_send < form_length){
+        for(var aux_send = 0; aux_send < form_length; aux_send++){
           $url_send_form = $url_send_form + "&valor" + aux_send + "=" + document.getElementById(aux_send).value;
           aux_send = aux_send + 1;
         }
@@ -110,27 +109,7 @@
         }
         alert("is_form_ok");
         if(is_form_ok){
-          //stablihing new XMLHttpRequest
-          var xhttp;
-          xhttp = new XMLHttpRequest();
-          //vendo o tamanho da form
-          alert(form_length);
-          //building the URL that will be send
-          url_send_form = "disp_form/answer.php?id_dispositivo="+id_dispositivo_select;
-          for(var aux_send = 0; aux_send < form_length; aux_send++){
-            url_send_form = url_send_form + "&valor" + aux_send + "=" + document.getElementById(aux_send).value;
-            aux_send = aux_send + 1;
-          }
-          alert(url_send_form);
-          //inicializing the request
-          //xhttp.onreadystatechange = function() {
-          //  if (this.readyState == 4 && this.status == 200) {
-          //    document.getElementById("answer_form").innerHTML = this.responseText;
-          //  }
-          //};
-          //sending the data to answer.
-          //xhttp.open("GET", $url_send_form, true);
-          //xhttp.send();  
+          sendForm(id_dispositivo_select);
         }
       }
 
