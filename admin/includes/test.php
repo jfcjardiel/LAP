078ff352afb5f3a9a3542f3933c3fb9ad06b6b9e
jardielPC <jfcjardiel@gmail.com>
2017-09-15 00:23:00 -0300

    Tentando pegar login

--- admin/includes/test.php
+++ admin/includes/test.php
@@ -1,8 +1,16 @@
 <?php
 
-$mysqli = new mysqli( "localhost", "sec_user", "secuserinput212", "secure_login");
+$mysqli = new mysqli('localhost', 'sec_user', 'secuserinput212', 'secure_login');
+
+//If there is any error, then show...
+if ($mysqli->connect_errno) {
+    // I do not know what to show yet
+    echo "Connection Problem";
+    exit;
+}
 
 $id = $mysqli->query("SELECT username FROM members");
 
 echo $id;
+
 ?>
