49b3c364bb35ae004d19d538c6efe1debbba4271
jardielPC <jfcjardiel@gmail.com>
2017-09-15 00:08:41 -0300

    Tentando pegar login

--- admin/includes/db_connect.php
+++ admin/includes/db_connect.php
@@ -1,10 +1,4 @@
 <?php
-include_once 'psl-config.php'; //incluindo a função php do include
-$conn_login = mysql_connect(HOST, USER, PASSWORD);
-if(!$conn_login){
-	die("Connection failed: " . mysql_error());
-}
-$conn_mysql = mysql_select_db(DATABASE);
-if(!$conn_mysql){
-	die("Could not connect to database: " . mysql_error());
-}
+include_once 'psl-config.php';   // As functions.php is not included
+
+$mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
