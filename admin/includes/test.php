d931f6486d8784f97a28641f8c2790db089801d1
jardielPC <jfcjardiel@gmail.com>
2017-09-15 00:20:21 -0300

    Tentando pegar login

--- admin/includes/test.php
+++ admin/includes/test.php
@@ -1,15 +1,6 @@
 <?php
-define("HOST", "localhost"); //o host que queremos conectar na database
-define("USER","sec_user"); //o nome do user
-define("PASSWORD", "secuserinput212"); // a senha do usuário!
-define("DATABASE", "secure_login"); //o nome da database que vamos querer acessar
 
-define("CAN_REGISTER", "any");
-define("DEFAULT_ROLE","member");
-
-define("SECURE", FALSE); //for development only
-
-$mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
+$mysqli = new mysqli( "localhost", "sec_user", "secuserinput212", "secure_login");
 
 $id = $mysqli->query("SELECT username FROM members");
 
