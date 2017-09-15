2ec4bdb7f6e5f55482d8a9f65657b116c2323f7d
jardielPC <jfcjardiel@gmail.com>
2017-09-15 00:15:59 -0300

    Tentando pegar login

--- admin/includes/test.php
+++ admin/includes/test.php
@@ -1,6 +1,15 @@
 <?php
-include_once 'db_connect.php';
-include_once 'psl-config.php';
+define("HOST", "localhost"); //o host que queremos conectar na database
+define("USER","sec_user"); //o nome do user
+define("PASSWORD", "secuserinput212"); // a senha do usuário!
+define("DATABASE", "secure_login"); //o nome da database que vamos querer acessar
+
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE","member");
+
+define("SECURE", FALSE); //for development only
+
+$mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
 
 $id = $mysqli->query("SELECT username FROM members");
 
