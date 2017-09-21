8f16e56ad89db7f5ed2c8eb536128351c8432df5
jardielPC <jfcjardiel@gmail.com>
2017-09-21 01:54:46 -0300

    Alteracoes na geracao dos nomes, novos testes

--- disp_form/input.php
+++ disp_form/input.php
@@ -47,12 +47,12 @@ while ($dispositivo = $result->fetch_assoc()) {
 //*******************************//
 
 //criando o upload  -> o arquivo vai ter o nome do dispositivo na pasta disp_form
-$target_dir_img = "./disp_form/img/";
+$target_dir_img = "disp_form/img/";
 $target_img = $target_dir_img . $dispositivo['nome_dispositivo'] . $id . ".jpg";
 
-if(file_exists($target_img)){
+//if(file_exists($target_img)){
     echo '<div class="blogimg_container"><a class="blog_img"><img alt="img" src="'.$target_img.'"></a></div>';
-}
+//}
 
 //*******************************//
 //***** INICIALIZE FORM *********//
