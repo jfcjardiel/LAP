5a4cdcfa1d2b0d736883dd88d6fca574a8b40076
jardielPC <jfcjardiel@gmail.com>
2017-09-25 03:27:37 -0300

    Primeira vez tentando o site completo

--- admin/includes/test.php
+++ admin/includes/test.php
@@ -11,7 +11,7 @@ while(!file_exists($image_result_server) && ($aux_time < 30)){
     $aux_time = $aux_time + 1;
 }
 
-$image_result = "../../disp_form/results/";
+$image_result = "disp_form/results/". $email_result[0] . $id_dispositivo . ".jpg";
 if($aux_time < 30){
     echo '<img alt="img" src="'.$image_result.'">';
 }else{
