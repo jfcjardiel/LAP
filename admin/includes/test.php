00edd9b5ec2462e4e1d375e981748a33887fbd73
jardielPC <jfcjardiel@gmail.com>
2017-09-25 03:26:27 -0300

    Primeira vez tentando o site completo

--- admin/includes/test.php
+++ admin/includes/test.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL);
 
 $aux_time = 0;
 $image_result_server = "/var/www/html/disp_form/results/jfcjardiel1.jpg";
-while(!file_exists($image_result_server) || ($aux_time < 30)){
+while(!file_exists($image_result_server) && ($aux_time < 30)){
     sleep(1);
     echo file_exists($image_result_server);
     $aux_time = $aux_time + 1;
