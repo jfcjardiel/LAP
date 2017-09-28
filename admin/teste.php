609e17af119dda7d21705b07890105f83bf8f5f1
jardielPC <jfcjardiel@gmail.com>
2017-09-28 15:39:46 -0300

    Criando ferramenta de modificacao

--- admin/teste.php
+++ admin/teste.php
@@ -6,13 +6,14 @@
 <h1>The XMLHttpRequest Object</h1>
 <button type="button" onclick="loadDoc()">Change Content</button>
 </div>
+<div id="oi"> </div>
 
 <script>
 function loadDoc() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("demo").innerHTML =
+      document.getElementById("oi").innerHTML =
       this.responseText;
     }
   };
