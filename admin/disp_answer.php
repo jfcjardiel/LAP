ec903520af926c6737150c366fdbb21ca752f9ed
jardielPC <jfcjardiel@gmail.com>
2017-09-29 16:38:01 -0300

    Criando ferramenta de modificacao

--- admin/disp_answer.php
+++ admin/disp_answer.php
@@ -1,26 +1,60 @@
 <?php
 //getting 
 $id_dispositivo = $_REQUEST["id_dispositivo"] + 0;
+//echo $id_dispositivo;
+
+//***********************************//
+//******** START CONNECTION *********//
+//***********************************//
+
+//Starting connection
+$mysqli_disp = new mysqli('localhost', 'root', 'input212', 'input');
+
+//If there is any error, then show...
+if ($mysqli_disp->connect_errno) {
+    // I do not know what to show yet
+    echo "<h2 class='blog_title'>Connection Problem </h2>";
+    exit;
+}
+
+//**********************************//
+//******** READING DATABSE *********//
+//**********************************//
+
+//We are getting all the attributes of the dispositivo
+$sql = "SELECT id_config FROM config_dispositivo_atributos WHERE id_dispositivo=" . $id_dispositivo;
+if (!$result = $mysqli_disp->query($sql)) {
+    // I do not know what to show yet
+    echo "<h2 class='blog_title'>Connection Problem </h2>";
+    exit;
+}
+
+// If there is no result
+if ($result->num_rows === 0) {
+    // I do not know what to show yet
+    echo "<h2 class='blog_title'>Connection Problem </h2>";
+    exit;
+}
 
 //********************************************//
 //******** WRITING VALUES ON DATABSE *********//
 //********************************************//
 $aux_write = 0;
 
-if(isset($_REQUEST("nome_dispositivo"))) {
-    echo $_REQUEST("nome_dispositivo");
+if(isset($_REQUEST["nome_dispositivo"])) {
+    echo $_REQUEST["nome_dispositivo"];
 }
 
-if(isset($_REQUEST("show"))){
-    echo $_REQUEST("show");
+if(isset($_REQUEST["show"])){
+    echo $_REQUEST["show"];
 }
 
 //em cada id_config vamos escrever o valor relativo em valor_dispositivo_atributos
 $aux = 0;
 while ($aux < 30){
     $dispositivo_name = "valor".$aux;
-    if(isset($_REQUEST($dispositivo_name))){
-        echo $_REQUEST($dispositivo_name);
+    if(isset($_REQUEST[$dispositivo_name])){
+        echo $_REQUEST[$dispositivo_name];
     }
     $aux = $aux +1;
 }
