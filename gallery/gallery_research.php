708f12022529d92f17cf1b7eee60eddff4513b50
jardielPC <jfcjardiel@gmail.com>
2017-10-04 17:19:48 -0300

    Add gallery

---
+++ gallery/gallery_research.php
@@ -0,0 +1,11 @@
+<?php
+$id = 1;
+//criando o upload  -> o arquivo vai ter o nome do dispositivo na pasta disp_form
+$target_dir_img = "/var/www/html/gallery/gallery_research/";
+$target_img_server = $target_dir_img . $id . ".jpg";
+$target_img = "gallery/gallery_research/". $id . ".jpg";
+
+if(file_exists($target_img_server)){
+    echo '<a href="'.$target_img.'" title="This is Title"><img class="gallery_img" src="'.$target_img.'" alt="img" /><span class="view_btn">View</span></a>>';
+}
+?>
