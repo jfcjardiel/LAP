8b9da1b0312af0431fb49decfd44a8536a740e44
jardielPC <jfcjardiel@gmail.com>
2017-11-03 16:07:11 -0200

    Tentativa edicao professor

--- admin/prof_answer.php
+++ admin/prof_answer.php
@@ -60,38 +60,32 @@ if(isset($_POST[$year_art]) && $_POST[$year_art] != ""){
     }
 }
 
-// If there is no result
-if ($result->num_rows === 0) {
-    // I do not know what to show yet
-    echo "<h2 class='blog_title'>Connection Problem </h2>";
-    exit;
-}
-
 //*****************************************//
 //********** CHANGING REFERENCE ***********//
 //*****************************************//
 
-$num_articles = $num_articles - 1;
-
-//em cada id_config vamos escrever o valor relativo em valor_dispositivo_atributos
-while ($articles = $result->fetch_assoc()){
-    $year_art = 'year'.$num_articles;
-    $ref_art = 'art'.$num_articles;
-    if(isset($_POST[$year_art])){
-        if($_POST[$year_art] != ""){
-            $sql_write = 'UPDATE articles SET year='.$year_art.' WHERE id_art='.$articles['id_art'];
-            if(!$result_write = $mysqli_information->query($sql_write)){
-                echo "<p>Connection Problem writing</p>";
-                exit;
+if ($num_articles > 1) {
+    $num_articles = $num_articles - 1;
+    //em cada id_config vamos escrever o valor relativo em valor_dispositivo_atributos
+    while ($articles = $result->fetch_assoc()){
+        $year_art = 'year'.$num_articles;
+        $ref_art = 'art'.$num_articles;
+        if(isset($_POST[$year_art])){
+            if($_POST[$year_art] != ""){
+                $sql_write = 'UPDATE articles SET year='.$year_art.' WHERE id_art='.$articles['id_art'];
+                if(!$result_write = $mysqli_information->query($sql_write)){
+                    echo "<p>Connection Problem writing</p>";
+                    exit;
+                }
             }
         }
-    }
-    if(isset($_POST[$year_art])){
-        if($_POST[$year_art] != ""){
-            $sql_write = 'UPDATE articles SET reference="'.$ref_art.'" WHERE id_art='.$articles['id_art'];
-            if(!$result_write = $mysqli_information->query($sql_write)){
-                echo "<p>Connection Problem writing</p>";
-                exit;
+        if(isset($_POST[$year_art])){
+            if($_POST[$year_art] != ""){
+                $sql_write = 'UPDATE articles SET reference="'.$ref_art.'" WHERE id_art='.$articles['id_art'];
+                if(!$result_write = $mysqli_information->query($sql_write)){
+                    echo "<p>Connection Problem writing</p>";
+                    exit;
+                }
             }
         }
     }
