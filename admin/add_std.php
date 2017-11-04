cc4b2f0987620c11fb511ff5581c15d935690ad7
jardielPC <jfcjardiel@gmail.com>
2017-11-04 21:16:45 -0200

    Tentativa edicao professor

--- admin/add_std.php
+++ admin/add_std.php
@@ -14,9 +14,9 @@ sec_session_start();
 </head>
 
 <body bgcolor="#FFFFFF">
-<form method="post" attribute="post" action="std_answer.php?id_std=' . $id_std . '" enctype="multipart/form-data" id="mod_disp" name="mod_disp">
+<form method="post" attribute="post" action="add_std_final.php" enctype="multipart/form-data" id="mod_disp" name="mod_disp">
     <p> Student Name: </p>
-    <input type='text' name='name_std' placeholder=' ". $name_std ."' maxlength='30'><br>"
+    <input type='text' name='name_std' placeholder='Student Name' maxlength='30'><br>
     <p>Students Degree in LAP:</p>
     <input type="checkbox" name="phd" value="true"> <a>Phd Student</a>
     <input type="text" name="year_phd" placeholder="Degree Year"><br>
