9c2de7b5301aca7fe41b59676a389098ef626062
jardielPC <jfcjardiel@gmail.com>
2017-11-04 13:53:01 -0200

    Tentativa edicao professor

--- admin/prof_answer.php
+++ admin/prof_answer.php
@@ -26,6 +26,22 @@ if ($mysqli_information->connect_errno) {
     exit;
 }
 
+//***********************************//
+//******** READING DATABASE *********//
+//***********************************//
+
+//If there isnt any error, then lets read the sql content
+$sql = "SELECT * FROM articles WHERE id_prof=" . $id_prof . " ORDER BY year DESC";
+if (!$result = $mysqli_information->query($sql)) {
+    // I do not know what to show yet
+    echo "<p>Connection Problem </p>";
+    exit;
+}
+
+//the number of articles is the result of the research
+$num_articles = $result->num_rows+1;
+
+
 echo "<h1> Upload ok! </h1>";
 
 echo "<a href='edit_professor.php'>GO BACK</a>";
