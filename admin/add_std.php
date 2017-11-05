b22eec7d26df60e9c42dce525aa6adde529dcea7
jardielPC <jfcjardiel@gmail.com>
2017-11-05 00:43:04 -0200

    Tentativa edicao aluno

--- admin/add_std.php
+++ admin/add_std.php
@@ -11,10 +11,26 @@ sec_session_start();
 <html>
     <meta charset="utf-8">
 <head>
+<script type="text/javascript">
+    //function to know if the space is empty or not
+    function validateForm(){
+        //o .length conta o botao tambem
+        var name_std = document.forms["new_std"]["name_std"];
+        var about_std = document.forms["new_std"]["about_std"];
+        if(name_std == ""){
+          alert("Name must be filled out");
+          return false;          
+        }
+        if(about_std == ""){
+          alert("About must be filled out");
+          return false;             
+        }
+    }
+</script>
 </head>
 
 <body bgcolor="#FFFFFF">
-<form method="post" attribute="post" action="add_std_final.php" enctype="multipart/form-data" id="mod_disp" name="mod_disp">
+<form method="post" attribute="post" onsubmit="return validateForm()" action="add_std_final.php" enctype="multipart/form-data" id="new_std" name="new_std">
     <p> Student Name: </p>
     <input type='text' name='name_std' placeholder='Student Name' maxlength='30'><br>
     <p>Students Degree in LAP:</p>
@@ -24,10 +40,11 @@ sec_session_start();
     <input type="text" name="year_master" placeholder="Degree Year"><br>
     <input type="checkbox" name="grad" value="true"> <a>Undergraduate Student</a>
     <input type="text" name="year_grad" placeholder="Degree Year"><br>
+    <input type="checkbox" name="std_active" value="true"> <a>Is the student working in LAP?</a>
     <p>About the student: </p>
     <textarea cols="60" rows="10" placeholder="place here information about the student" name="about_std"></textarea><br>
     <p>Here is the IMAGE you are going to upload (JUST JPG):</p>
-    <input type = "file" name="std_img" id = "std_imgd"><br><br>
+    <input type = "file" name="std_img" id = "std_img"><br><br>
     <button type="submit" name="change_std" id="change_std" value="change_std">Change Information</br></button> 
 </form>
 </body>
--- admin/add_std_final.php
+++ admin/add_std_final.php
@@ -32,59 +32,148 @@ if ($mysqli_information->connect_errno) {
 //******************************************//
 
 //updating name_prof
-if(isset($_POST["name_prof"])) {
-    if($_POST["name_prof"] != ""){
-        $sql_write = "UPDATE professors SET name_prof='". $_POST["name_prof"] ."' WHERE id_prof=".$id_prof;
-        if(!$result_write = $mysqli_information->query($sql_write)){
-            echo "<p>Connection Problem writing</p>";
+if(isset($_POST["name_std"])) {
+    if($_POST["name_std"] == ""){
+            echo "<p>Problem writing</p>";
             exit;
-        }
+    }else{
+        $name_std = $_POST["name_std"];
     }
 }
 
-//updating show_professor
-if(isset($_POST["show_professor"])){
-    if($_POST["show_professor"] == "yes"){
-        $sql_write = "UPDATE professors SET show_professor=TRUE WHERE id_prof=".$id_prof;
-        if(!$result_write = $mysqli_information->query($sql_write)){
-            echo "<p>Connection Problem writing</p>";
-            exit;
-        }
+//updating PHD
+if(isset($_POST["phd"])){
+    if($_POST["phd"] == "true"){
+        $phd = "TRUE";
+    }else{
+        $phd = "FALSE";
     }
-    if($_REQUEST["show_professor"] == "no"){
-        $sql_write = "UPDATE professors SET show_professor=FALSE WHERE id_prof=".$id_prof;
-        if(!$result_write = $mysqli_information->query($sql_write)){
-            echo "<p>Connection Problem writing</p>";
-            exit;
-        }
+    if($_REQUEST["year_phd"] == ""){
+        $year_phd= "NULL";
+    }else{
+        $year_phd = $_POST["year_phd"];
     }
 }
 
-//updating email_prof
-if(isset($_POST["email_prof"])) {
-    if($_POST["email_prof"] != ""){
-        $sql_write = "UPDATE professors SET email='". $_POST["email_prof"] ."' WHERE id_prof=".$id_prof;
-        if(!$result_write = $mysqli_information->query($sql_write)){
-            echo "<p>Connection Problem writing</p>";
-            exit;
-        }
+if(isset($_POST["year_phd"])){
+    if($_REQUEST["year_phd"] == ""){
+        $year_phd= "NULL";
     }
 }
 
-//updating about_prof
-if(isset($_POST["about_prof"])) {
-    if($_POST["about_prof"] != ""){
-        $sql_write = "UPDATE professors SET about_prof='". $_POST["about_prof"] ."' WHERE id_prof=".$id_prof;
-        if(!$result_write = $mysqli_information->query($sql_write)){
-            echo "<p>Connection Problem writing</p>";
-            exit;
-        }
+//updating master
+if(isset($_POST["master"])){
+    if($_POST["master"] == "true"){
+        $master = "TRUE";
+    }else{
+        $master = "FALSE";
+    }
+    if($_REQUEST["year_master"] == ""){
+        $year_master= "NULL";
+    }else{
+        $year_master = $_POST["year_master"];
     }
 }
 
-echo "<h1> Upload ok! </h1>";
+if(isset($_POST["year_master"])){
+    if($_REQUEST["year_master"] == ""){
+        $year_master= "NULL";
+    }
+}
 
-echo "<a href='edit_professor.php'>GO BACK</a>";
+//updating grad
+if(isset($_POST["grad"])){
+    if($_POST["grad"] == "true"){
+        $grad = "TRUE";
+    }else{
+        $grad = "FALSE";
+    }
+    if($_REQUEST["year_grad"] == ""){
+        $year_grad= "NULL";
+    }else{
+        $year_grad = $_POST["year_grad"];
+    }
+}
+
+if(isset($_POST["year_grad"])){
+    if($_REQUEST["year_grad"] == ""){
+        $year_grad= "NULL";
+    }
+}
+
+//getting active
+if(isset($_POST["std_active"])){
+    if($_POST["std_active"] == "true"){
+        $active = "TRUE";
+    }else{
+        $active = "FALSE";
+}
+
+//getting about_std
+if(isset($_POST["about_std"])) {
+    if($_POST["about_std"] == ""){
+            echo "<p>Problem writing</p>";
+            exit;
+    }else{
+        $about_std = $_POST["about_std"];
+    }
+}
+
+$sql_write = "INSERT INTO students (id_std, name_std, php, year_phd, master, year_master, grad, year_grad, active, about_std) VALUES (NULL,".$name_std.",".$php.",".$year_php.",".$master.",".$year_master.",".$grad.",".$year_grad.",".$active.",".$about_std.")";
+if(!$result_write = $mysqli_information->query($sql_write)){
+    echo "<p>Connection Problem writing</p>";
+    exit;
+}
+
+$sql_write = "";
+if(!$result_write = $mysqli_information->query($sql_write)){
+    echo "<p>Connection Problem writing</p>";
+    exit;
+}
+
+//**************************************//
+//******** UPLOADING NA FIGURA *********//
+//**************************************//
+
+//If there isnt any error, then lets read the sql content
+$sql = "SELECT id_std FROM students ORDER BY id_std DESC LIMIT 1";
+if (!$result = $mysqli_information->query($sql)) {
+    // I do not know what to show yet
+    echo "<p>Connection Problem </p>";
+    exit;
+}
+
+// If there is no result (don't apply in this case)
+if ($result->num_rows === 0) {
+    // I do not know what to show yet
+    echo "<p>Connection Problem </p>";
+    exit;
+}
+
+//taking all students informations
+$std_row = $result->fetch_assoc();
+$id_std = $std_row['id_std'];
+
+//criando o upload  -> o arquivo vai ter o nome do dispositivo na pasta disp_form
+$target_dir_img = "/var/www/html/img/std";
+$target_img = $target_dir_img . $id_std . ".jpg";
+
+if($_FILES['std_img']["error"] == 0){
+    $img_name = $_FILES['std_img']['name'];
+    $img_size = $_FILES['std_img']['size'];
+    $img_tmp = $_FILES['std_img']['tmp_name'];
+    $img_type = $_FILES['std_img']['type'];
+    $img_extension = strtolower(end(explode('.',$file_name)));
+    $moved = move_uploaded_file($img_tmp,$target_img);
+    if(!$moved){
+        echo "<p> There was an error, image not uploaded";
+        exit;
+    }
+}
+
+echo "<h1> Student Registered! </h1>";
+
+echo "<a href='edit_students.php'>GO BACK</a>";
 
 //we should close the connection
 $mysqli_information->close();
