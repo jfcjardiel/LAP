78d405be2ee04d3b9e2af73ae3cd4244b8dbc0e9
Jardiel <jfcjardiel@gmail.com>
2019-02-06 15:48:44 -0200

    Atualização Daniel

--- articles/general.php
+++ articles/general.php
@@ -65,53 +65,7 @@ if ($num_articles > 0) {
 ?>
 </table>
 
-<table class="table table-striped course_table">
-        <thead>
-          <tr>          
-            <th>Conferences</th>
-            <th></th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
 
-<?php
-
-//********************************//
-//***** READING ARTICLES *********//
-//********************************//
-
-//If there isnt any error, then lets read the sql content
-$sql = "SELECT * FROM articles WHERE art_group=TRUE AND art_conference=TRUE ORDER BY year DESC";
-if (!$result = $mysqli_information->query($sql)) {
-    // I do not know what to show yet
-    echo "<p>Connection Problem </p>";
-    exit;
-}
-
-//getting the number of articles
-$num_articles = $result->num_rows;
-$art_year = 0;
-
-if ($num_articles > 0) {
-    //Writing the form options
-    while ($config_dispositivo = $result->fetch_assoc()) {
-        //it is exibitig the line.
-        echo '<tr><td>';
-        if($art_year != $config_dispositivo['year']){
-          echo $config_dispositivo['year'];
-          $art_year = $config_dispositivo['year'];
-        }
-        echo "</td>";
-        echo '<td>['.$num_articles.']</td><td>';
-        echo $config_dispositivo['reference'];
-        echo  '</td></tr>';
-        $num_articles = $num_articles - 1;
-    }
-}
-
-?>
-</table>
 
 <table class="table table-striped course_table">
         <thead>
@@ -160,3 +114,54 @@ if ($num_articles > 0) {
 
 ?>
 </table>
+
+
+
+<table class="table table-striped course_table">
+        <thead>
+          <tr>          
+            <th>Conferences</th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+
+<?php
+
+//********************************//
+//***** READING ARTICLES *********//
+//********************************//
+
+//If there isnt any error, then lets read the sql content
+$sql = "SELECT * FROM articles WHERE art_group=TRUE AND art_conference=TRUE ORDER BY year DESC";
+if (!$result = $mysqli_information->query($sql)) {
+    // I do not know what to show yet
+    echo "<p>Connection Problem </p>";
+    exit;
+}
+
+//getting the number of articles
+$num_articles = $result->num_rows;
+$art_year = 0;
+
+if ($num_articles > 0) {
+    //Writing the form options
+    while ($config_dispositivo = $result->fetch_assoc()) {
+        //it is exibitig the line.
+        echo '<tr><td>';
+        if($art_year != $config_dispositivo['year']){
+          echo $config_dispositivo['year'];
+          $art_year = $config_dispositivo['year'];
+        }
+        echo "</td>";
+        echo '<td>['.$num_articles.']</td><td>';
+        echo $config_dispositivo['reference'];
+        echo  '</td></tr>';
+        $num_articles = $num_articles - 1;
+    }
+}
+
+?>
+</table>
+
